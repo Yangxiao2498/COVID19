b084bab1f375c2aa39bd524c6ecaa2d5c221af8b
yang xiao <65546468+Yangxiao2498@users.noreply.github.com>
2020-06-04 13:21:31 -0400

    Update plotSimulation.py
    display data of the end of simulation

--- plotSimulation.py
+++ plotSimulation.py
@@ -13,9 +13,9 @@ def plotting_SIR_Simulation(S, I, R ,N,t,simulation_period,deaths):
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Percentage of Population (%)')
     ax.set_ylim(0, 100)
-    st.write('Total infected cases will be ',int(I[simulation_period]))
-    st.write('Total recoverd cases will be ',int(R[simulation_period]))
-    st.write('Total deaths will be ',int(I[simulation_period]*deaths))
+    st.write('Total infected cases will be ',int(I[499]))
+    st.write('Total recoverd cases will be ',int(R[499]))
+    st.write('Total deaths will be ',int(I[499]*deaths))
     #ax.vlines(x=simulation_period,ymin=0,ymax=100,linewidth=2, color='k',label='Intersection')
     legend = ax.legend()
     for spine in ('top', 'right', 'bottom', 'left'):
@@ -30,7 +30,7 @@ def plotting_SIR_Infection(S, I, R ,N,t,simulation_period):
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Total Infected')
     #ax.set_ylim(0, I)
-    st.write('Total infected cases will be ',int(I[simulation_period]))
+    st.write('Total infected cases will be ',int(I[499]))
     #ax.vlines(x=simulation_period,ymin=0,ymax=I,linewidth=2, color='k',label='Intersection')
     legend = ax.legend()
     for spine in ('top', 'right', 'bottom', 'left'):
@@ -46,7 +46,7 @@ def plotting_SIR_Susceptible(S, I, R ,N,t,simulation_period):
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Total Susceptible)')
     #ax.set_ylim(0, I)
-    st.write('Total Susceptible cases will be ',int(S[simulation_period]))
+    st.write('Total Susceptible cases will be ',int(S[499]))
     legend = ax.legend()
     for spine in ('top', 'right', 'bottom', 'left'):
         ax.spines[spine].set_visible(False)
@@ -60,7 +60,7 @@ def plotting_SIR_Recovery(S, I, R ,N,t,simulation_period):
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Total Recovery)')
     #ax.set_ylim(0, I)
-    st.write('Total Recovered cases will be ',int(R[simulation_period]))
+    st.write('Total Recovered cases will be ',int(R[499]))
     legend = ax.legend()
     for spine in ('top', 'right', 'bottom', 'left'):
         ax.spines[spine].set_visible(False)
@@ -75,8 +75,8 @@ def plotting_SIR_IR(S, I, R ,N,t,simulation_period):
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Total Infected)')
     #ax.set_ylim(0, I)
-    st.write('Total Infected cases will be ',int(I[simulation_period]))
-    st.write('Total Recovered cases will be ',int(R[simulation_period]))
+    st.write('Total Infected cases will be ',int(I[499]))
+    st.write('Total Recovered cases will be ',int(R[499]))
     legend = ax.legend()
     for spine in ('top', 'right', 'bottom', 'left'):
         ax.spines[spine].set_visible(False)
